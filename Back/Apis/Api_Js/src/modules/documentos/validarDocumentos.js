5ed2ccca821190b435a6a5ed76a7a1e0475b68ed
neo <sarmientodiazluisangel@gmail.com>
2026-01-28 21:17:31 -0500

    Dinamo version 2.1.1.1 -> Se modifican las funciones del apartado de firmas

--- Back/Apis/Api_Js/src/modules/documentos/validarDocumentos.js
+++ Back/Apis/Api_Js/src/modules/documentos/validarDocumentos.js
@@ -5,79 +5,81 @@ import signerPkg from '@signpdf/signer-p12';
 import { plainAddPlaceholder } from '@signpdf/placeholder-plain';
 import { PDFDocument } from 'pdf-lib';
 import forge from 'node-forge';
-
 const { SignPdf } = signpdfPkg;
 const { P12Signer } = signerPkg;
 
+/*=================================================
+    FUNCIONES QUE SE ENCARGAN DE GESTIONAR
+    LAS FIRMAS, LA VALIDACION DE DOCUMENTOS 
+    JUNTO CON SUS CERTIFICADOS Y LA OBTENCION
+    DE DATOS DE LOS MISMOS
+=================================================*/ 
 
-/* Verificar si es un PDF y si tiene certificados*/
+/* Verificar si el archivos es un PDF y si tiene firmas digitales. */
 export const verificarArchivo = async({bufferPdf}) => {
     try {
+
+        /* Verificar si es un pdf */
         if (!bufferPdf.slice(0, 5).toString() === '%PDF-') {
             throw new Error("El archivo no es un pdf");
         }
-        console.log("Es un pdf"); 
 
+        /* Objetos de firmas */
+        let arrayFirmas = []; 
+
+        /* Leer el contendio raw del pdf */
         const pdfDoc = await PDFDocument.load(bufferPdf);
-        
-        // Para verificar firmas, necesitas leer el contenido raw
         const pdfBuffer = Buffer.from(bufferPdf);
         const pdfString = pdfBuffer.toString('binary');
-        //console.log(pdfString)
-        
-        //
-        // Buscar el diccionario de firmas
-        const tienefirma = pdfString.includes('/Type /Sig') || pdfString.includes('/SubFilter /adbe.pkcs7');
-        console.log(tienefirma)
+        /*await fs.writeFileSync("./buffer.txt", pdfString,{encoding:"utf-8"}); */
 
-        // Extraer información de la firma (simplificado)
-        if (tienefirma) {
-          const firmaMatch = pdfString.match(/\/Contents\s*<([^>]+)>/);
-          if (firmaMatch) {
-            const firmaHex = firmaMatch[1];
-            let hexLimpio = firmaHex
-            .replace(/\s/g, '')           // Eliminar espacios
-            .replace(/\n/g, '')           // Eliminar saltos de línea
-            .replace(/\r/g, '')           // Eliminar retornos de carro
-            .replace(/[^0-9A-Fa-f]/g, '') // Solo caracteres hexadecimales
-            .toUpperCase();
-
-            if (hexLimpio.length % 2 !== 0) {
-              console.warn('⚠️ Longitud impar detectada, agregando 0 al inicio');
-              hexLimpio = '0' + hexLimpio;
-            }
-            //console.log(hexLimpio)
-            const firmaBytes = forge.util.hexToBytes(hexLimpio);
-            console.log(firmaBytes.length); 
-
-            // Parsear la firma (con manejo de bytes sobrantes)
-            let p7 = null;
-            let longitudUsada = firmaBytes.length;
-
-            // Intentar parsear, reduciendo longitud si hay bytes extras
-            for (let len = firmaBytes.length; len > 100; len -= 10) {
-              try {
-                const bytesRecortados = firmaBytes.substring(0, len);
-                const asn1 = forge.asn1.fromDer(bytesRecortados);
-                p7 = forge.pkcs7.messageFromAsn1(asn1);
-                longitudUsada = len;
-                break;
-              } catch (e) {
-                // Continuar intentando con menos bytes
-                if (len <= 100) {
-                  throw new Error('No se pudo parsear la firma PKCS#7');
-                }
-              }
-            }
-
-            if (!p7) {
-              throw new Error('No se pudo decodificar la firma');
-            }
-
-
-            console.log(p7)
-          }
+        /* Buscar el diccionario de firmas */
+        const tienefirma = pdfString.includes('/Type/Sig') || pdfString.includes('/SubFilter/adbe.pkcs7');
+        if(!tienefirma){
+            /*console.log("No tiene firmas");*/
+            return true
         }
+
+        /* Extraer informacion de las firmas */
+        const regexFirmas = /\/Contents\s*<([0-9A-Fa-f]+)>/g;
+        let match;
+        let numeroFirma = 1;
+
+        while ((match = regexFirmas.exec(pdfString)) !== null){
+            let firmaProcesada = await procesarFirmas({firmaBinario:match[1]});
+            /*await fs.writeFileSync("./firmasBinario.txt", firmaProcesada,{encoding:"utf-8", flag:"a"});*/
+            console.log(firmaProcesada);
+        }
+
+
+        //     // 
+        //     let p7 = null;
+        //     let longitudUsada = firmaBytes.length;
+
+        //     // Intentar parsear, reduciendo longitud si hay bytes extras
+        //     for (let len = firmaBytes.length; len > 100; len -= 10) {
+        //       try {
+        //         const bytesRecortados = firmaBytes.substring(0, len);
+        //         const asn1 = forge.asn1.fromDer(bytesRecortados);
+        //         p7 = forge.pkcs7.messageFromAsn1(asn1);
+        //         longitudUsada = len;
+        //         break;
+        //       } catch (e) {
+        //         // Continuar intentando con menos bytes
+        //         if (len <= 100) {
+        //           throw new Error('No se pudo parsear la firma PKCS#7');
+        //         }
+        //       }
+        //     }
+
+        //     if (!p7) {
+        //       throw new Error('No se pudo decodificar la firma');
+        //     }
+
+
+        //     console.log(p7)
+        //   }
+        // }
 
         
         return true;
@@ -86,16 +88,58 @@ export const verificarArchivo = async({bufferPdf}) => {
     }
 }
 
-/* Obtener inforacion de las firmas digitales en los pdf */
-export const obtenerDatosFirmas = async({firmas}) => {
+/* Obtener la información de las firmas digitales encontradas en los pdf */
+export const procesarFirmas = async({firmaBinario}) => {
     try {
+        /* Modificar el string con el binario de la firma */
+        let firmaHexa = firmaBinario
+        .replace(/\s/g, '')
+        .replace(/\n/g, '')
+        .replace(/\r/g, '')
+        .replace(/[^0-9A-Fa-f]/g, '')
+        .toUpperCase();
+
+        if (firmaHexa.length % 2 !== 0) {
+            firmaHexa = '0' + firmaHexa;
+        }
+
+        /* Convertir a bytes */
+        const firmaBytes = forge.util.hexToBytes(firmaHexa);
+        const asn1 = forge.asn1.fromDer(firmaBytes);
         
+        // let p7 = null;
+        // let longitudUsada = firmaBytes.length;
+        
+        // /* Intentar parsear, reduciendo longitud si hay bytes extras */ 
+        // for (let len = firmaBytes.length; len > 100; len -= 10) {
+        //   try {
+        //     const bytesRecortados = firmaBytes.substring(0, len);
+        //     const asn1 = forge.asn1.fromDer(bytesRecortados);
+        //     p7 = forge.pkcs7.messageFromAsn1(asn1);
+        //     longitudUsada = len;
+        //     break;
+        //   } catch (e) {
+        //     /* Continuar intentando con menos bytes */
+        //     if (len <= 100) {
+        //       throw new Error('No se pudo parsear la firma PKCS#7');
+        //     }
+        //   }
+        // }
+
+        //console.log(firmaBytes)
+
+        // if (p7 === null ) {
+        //     throw new Error(`No se pudo decodificar la firma:${err}`);
+        // }
+        
+
+        return firmaBytes;
     } catch (error) {
-        throw new Error("Error al gestionar las firmas")
+        throw new Error(`Error al procesar la firma:${error.message}`); 
     }
 }
 
-/* Agregar un placeholder al documento pdf */
+/* Agregar un placeholder o espacio de firma al documento pdf */
 export const agregarPlaceholder = async ({bufferPdf}) =>{
     try {
         const pdfPlaceholder = await plainAddPlaceholder({
@@ -136,8 +180,8 @@ export const firmarPDF = async ({pdfPlaceholder, p12Buffer, p12Contrasena}) =>{
 /* Verificar firma digital */
 
 
-const p12Buffer = fs.readFileSync('C:/Users/sarmi/Desktop/PROYECTOS/TRABAJO/ACIEL/Dinamo/Docs/FirmasDigitales/lui.sarmiento/1023373788.p12');
-const pdfBuffer = fs.readFileSync('C:/Users/sarmi/Desktop/PROYECTOS/TRABAJO/ACIEL/Dinamo/Docs/DocumentosFirmas/documento-firmado.pdf');
+//const p12Buffer = fs.readFileSync('C:/Users/sarmi/Desktop/PROYECTOS/TRABAJO/ACIEL/Dinamo/Docs/FirmasDigitales/lui.sarmiento/1023373788.p12');
+const pdfBuffer = fs.readFileSync('/home/neo/Desktop/PROYECTS/Dinamo/Docs/DocumentosFirmas/F-ACS-01 ACTA-V2 27-01-2026.pdf');
 
 verificarArchivo({bufferPdf:pdfBuffer})
 .then((res)=>{
